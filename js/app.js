15f86a5a0d7fb6b0db222b302abdea24f7be95d1
MateoFontaine <Mateofontaine7@gmail.com>
2024-10-16 18:08:55 -0300

    Agregamos un spinner al enviar

--- js/app.js
+++ js/app.js
@@ -10,11 +10,32 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario')
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+    const spinner = document.querySelector('#spinner');
 
     inputEmail.addEventListener('input', validar);
     inputAsunto.addEventListener('input', validar);
     inputMensaje.addEventListener('input', validar);
+    
+    formulario.addEventListener('submit', enviarEmail);
 
+    btnReset.addEventListener('click', function(e){
+        e.preventDefault();
+
+        // reiniciar el objeto
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+        formulario.reset();
+        comprobarEmail();
+    })
+
+    function enviarEmail(e) {
+        e.preventDefault();
+
+        spinner.classList.add('flex');
+        spinner.classList.remove('hidden');
+    }
 
     function validar(e){
         if(e.target.value.trim() === ''){
